63c4b0acf6a1d9660a208aa7eec0caf108877507
sebastiansopala <s.sopala@gmail.com>
2023-08-20 13:20:40 +0200

    feat: add cart page link test

--- Pages/home_page.py
+++ Pages/home_page.py
@@ -8,6 +8,7 @@ class HomePage(BasePage):
     about_us_link = (By.XPATH, '//a[text()="About us"]')
     contact_link = (By.XPATH, '//a[text()="Contact"]')
     cart_link = (By.XPATH, '//a[text()="Cart"]')
+    place_order_btn = (By.XPATH, '//button[text()="Place Order"]')
 
     def open_contact(self):
         self.click_on(*self.contact_link)
@@ -17,6 +18,9 @@ class HomePage(BasePage):
 
     def open_cart(self):
         self.click_on(*self.cart_link)
+
+    def wait_for_cart_page_to_open(self):
+        self.wait_for_visible(*self.place_order_btn)
 
 
 
